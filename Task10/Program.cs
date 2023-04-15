7f9ff46be3b684fd0c93167b9a5ae82ca0356390
Yan <mamul.93@mail.ru>
2023-04-15 20:58:07 +0900

    10

---
+++ Task10/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Эта программа принимает на вход трехзначное число, на выходе показывает вторую цифру числа");
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine()!);
+
+int GetLastDigit(int number)
+{
+    return ((number / 10) % 10);
+}
+
+if (number <100 || number >= 1000)
+{
+   Console.WriteLine("Вы ввели не трехзначное число!");
+}
+else
+{
+   Console.WriteLine($"Вторая цифра числа {number} - {GetLastDigit(number)}");
+}
+
+
