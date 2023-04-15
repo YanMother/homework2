bdf815098bc0c5f68556431730e8ea149b85715b
Yan <mamul.93@mail.ru>
2023-04-15 21:47:17 +0900

    13

---
+++ Task13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int abc = int.Parse(Console.ReadLine()!);
+
+if (abc < 100)
+{
+   Console.WriteLine($"{abc} - нет третьей цифры");
+}
+else
+{
+   while (abc >= 999)
+   {
+        abc /= 10;
+   }
+        int firstDecimalPlace = abc % 10;
+    Console.WriteLine($"Третья цифра заданного числа: {firstDecimalPlace}");
+}
